764fbe28dbf52fe41e8701e9bfeac0a8c5c4a013
A <Arsema.arsemaagebremichael@gmail.com>
2025-05-07 09:02:39 +0300

    initial commit

--- parent_details.sql
+++ parent_details.sql
@@ -1,15 +1,19 @@
-create table parent_details(
+CREATE SCHEMA malnutrition;
+create table malnutrition.parent_details(
 	parent_id VARCHAR(10) primary key,
 	parent_name VARCHAR(150) not null,
 	contact_information VARCHAR (20) not null,
 	location VARCHAR(100) not null,
-	estimated_income VARCHAR not null
+	estimated_income VARCHAR not null,
+    gender VARCHAR (10),
+    national_id VARCHAR (10) 
 );
-alter table parent_details add column gender VARCHAR (10);
-alter table parent_details add column national_id VARCHAR (10);
-select * from parent_details;
 
-insert into parent_details (parent_id, parent_name, contact_information, location, estimated_income, gender, national_id)
+select * from malnutrition.parent_details;
+
+alter table malnutrition.parent_details add constraint unique_id unique (national_id);
+
+insert into malnutrition.parent_details (parent_id, parent_name, contact_information, location, estimated_income, gender, national_id)
 values ('P001', 'Zafi Saloi', '+25923987843', 'Latitude: 48.68429, Longitude: -139.18121', '2000ksh', 'Female', '30003188'),
 ('P002', 'Etenesh Hailu', '+25634223124', 'Latitude: 48.68429, Longitude: -139.18121', '4000ksh', 'Female', '41382178' ),
 ('P003', 'Helen Atieno', '+251953422389', 'Latitude: 59.68429, Longitude: -139.18121', '3000ksh', 'Female', '38779856' ),
@@ -27,13 +31,19 @@ values ('P001', 'Zafi Saloi', '+25923987843', 'Latitude: 48.68429, Longitude: -1
 ('P0016', 'Edith', '+25923987890', 'Latitude: 88.68429, Longitude: -139.18121', '2000ks', 'Female', '32548897' );
 
 
-select * from parent_details;
+select * from malnutrition.parent_details;
+
+insert into malnutrition.parent_details (parent_id, parent_name, contact_information, location, estimated_income, gender, national_id)
+values ('P0017', 'Karen Queen', '+25920987843', 'Latitude: 50.68429, Longitude: -139.18121', '4000ksh', 'Female', '39003188'),
+('P0019', 'kiyo Hailu', '+25634223123', 'Latitude: 48.68429, Longitude: -139.18121', '4000ksh', 'Male', '40982178' );
 
 
 --
-select * from parent_details;
+select * from malnutrition.parent_details;
 
 
 select COUNT(*) 
 	parent_name_start_with_k 
-	from parent_details where LEFT(parent_name, 1) in ('k', 'K'); 
+	from malnutrition.parent_details where LEFT(parent_name, 1) in ('k', 'K'); 
+    
+
